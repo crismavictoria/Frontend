1cb85d9da4aa4e13927158389fef79c46af73080
Crisma Victoria Tanola <ctanola@beeline.com>
2023-06-06 13:34:58 +0800

    Create encoder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: []
 })
 export class AppComponent {
-  title = 'Frontend';
+  text = '';
+  encoding = false;
+  encodedText = '';
+
+  private readonly apiUrl = 'https://localhost:5001/api/encode';
+
+  constructor(private http: HttpClient) {}
+
+  convert() {
+    this.encoding = true;
+    this.encodedText = '';
+
+    this.http.post<string>(this.apiUrl, this.text, { responseType: 'text' as 'json' })
+      .subscribe({
+        next: (character: string) => {
+          this.encodedText += character;
+        },
+        complete: () => {
+          this.encoding = false;
+        },
+        error: (error: any) => {
+          console.log(error);
+          this.encoding = false;
+        }
+      });
+  }
+
+  cancel() {
+    this.http.post<string>(`${this.apiUrl}/cancel`, {}).subscribe();
+  }
 }
